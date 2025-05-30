863814a293a3265fea85464dca7d326fda24389c
Sergio Ortiz <seortizblk@gmail.com>
2025-05-30 03:27:15 +0000

    configuraciones adicionales para docker

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+#IMAGE_NAME="fcom/promanager-base:latest"
+
+echo "🔧 Actualizando repositorio..."
+#git pull
+echo "✅ Repositorio actualizado."
+
+#echo "🔧 Creando imagen base..."
+#docker build -f docker/Dockerfile.base -t $IMAGE_NAME .
+#echo "✅ Imagen base creada."
+
+echo "🚀 Ejecutando docker-compose up..."
+docker compose -f docker-compose-prod.yml up --build -d
+echo "✅ Contenedores ejecutados."
+
+echo "🔄 Limpiando imágenes no utilizadas..."
+docker image prune -f
+echo "✅ Imágenes no utilizadas eliminadas."
+
+echo "Sistema iniciado correctamente."
